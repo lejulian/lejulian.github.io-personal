a3accf0c78bca495428bacd897d05f86e88b566c
lejulian <oleary.julian@gmail.com>
2024-12-26 18:27:13 -0800

    removed page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -26,9 +26,8 @@ const Index = () => (
         {' '}
         Welcome to my website. Please feel free to read more{' '}
         <Link to="/about">about me</Link>, or you can check out my{' '}
-        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{' '}
-        view <Link to="/stats">site statistics</Link>, or{' '}
-        <Link to="/contact">contact</Link> me.
+        <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,
+        or{' '}<Link to="/contact">contact</Link> me.
       </p>
 
     </article>
